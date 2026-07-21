6a1ea23be7ed8e55689d7a3a65efbd2ef801f71a
UlianaGritchina <ugritchina@icloud.com>
2026-07-21 13:14:12 +0300

    Initial Commit

---
+++ FinanceManagerTests/FinanceManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceManagerTests.swift
+//  FinanceManagerTests
+//
+//  Created by Ульяна Гритчина on 21.07.2026.
+//
+
+import Testing
+@testable import FinanceManager
+
+struct FinanceManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
